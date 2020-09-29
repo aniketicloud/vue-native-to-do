85f8dc9f135abb17762409df1daff7119e5c2062
Aniket <aniketicloud@gmail.com>
2020-09-29 10:32:52 +0530

    folder restructure

--- App.vue
+++ App.vue
@@ -1,43 +1,20 @@
 <template>
-  <view class="container">
-    <view class="btn-container">
-      <button title="show A" :on-press="() => handleBtnClick('A')" />
-      <button title="show B" :on-press="() => handleBtnClick('B')" />
-      <button title="show C" :on-press="() => handleBtnClick('C')" />
-    </view>
-    <view>
-      <text v-if="type === 'A'"> A </text>
-      <text v-else-if="type === 'B'"> B </text>
-      <text v-else-if="type === 'C'"> C </text>
-      <text v-else> NOT A/B/C </text>
-    </view>
-  </view>
+  <app-show-ABC></app-show-ABC>
 </template>
 
 <script>
+import ShowABC from "./components/examples/ShowABC";
 export default {
-  data() {
-    return {
-      type: Number,
-    };
+  components: {
+    appShowABC: ShowABC,
   },
 
-  methods: {
-    handleBtnClick(type) {
-      this.type = type;
-    },
+  data() {
+    return {};
   },
+
+  methods: {},
 };
 </script>
 
-<style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
-</style>
+<style></style>
---
+++ components/examples/ShowABC.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="container">
+    <view class="btn-container">
+      <button title="show A" :on-press="() => handleBtnClick('A')" />
+      <button title="show B" :on-press="() => handleBtnClick('B')" />
+      <button title="show C" :on-press="() => handleBtnClick('C')" />
+    </view>
+    <view>
+      <text v-if="type === 'A'"> A </text>
+      <text v-else-if="type === 'B'"> B </text>
+      <text v-else-if="type === 'C'"> C </text>
+      <text v-else> NOT A/B/C </text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: Number,
+    };
+  },
+
+  methods: {
+    handleBtnClick(type) {
+      this.type = type;
+    },
+  },
+};
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
