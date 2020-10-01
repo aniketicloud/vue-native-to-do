e1677266f7290fff7503c1903596e4fddcfe5738
Aniket <aniketicloud@gmail.com>
2020-10-01 22:36:16 +0530

    added Status Bar and Header

--- App.vue
+++ App.vue
@@ -1,14 +1,25 @@
 <template>
-  <hello-world></hello-world>
+  <view class="container">
+    <app-status-bar color="#ffce00" />
+    <app-main-header title="to-do-app"></app-main-header>
+  </view>
 </template>
 
 <script>
-import HelloWorld from "./components/examples/HelloWorld/HelloWorld";
+import StatusBar from "./components/status-bar/StatusBar";
+import MainHeader from "./components/mainheader/MainHeader";
+
 export default {
   components: {
-    HelloWorld,
+    appStatusBar: StatusBar,
+    appMainHeader: MainHeader,
   },
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  background-color: bisque;
+  flex: 1;
+}
+</style>
