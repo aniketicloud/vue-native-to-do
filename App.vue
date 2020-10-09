67ceac258d7a8d5ae1d07f748594162117f59fd5
Aniket <aniketicloud@gmail.com>
2020-10-09 11:07:10 +0530

    added input and Add button

--- App.vue
+++ App.vue
@@ -2,6 +2,13 @@
   <view class="container">
     <app-status-bar color="#ffce00" />
     <app-main-header title="to-do-app"></app-main-header>
+
+    <view class="inputContainer">
+      <text-input class="input" v-model="newTodoText"></text-input>
+      <touchable-opacity class="add-btn" :onPress="newTodo">
+        <text class="btn-text">ADD</text>
+      </touchable-opacity>
+    </view>
   </view>
 </template>
 
@@ -10,16 +17,52 @@ import StatusBar from "./components/status-bar/StatusBar";
 import MainHeader from "./components/mainheader/MainHeader";
 
 export default {
+  data() {
+    return {
+      newTodoText: "",
+    };
+  },
   components: {
     appStatusBar: StatusBar,
     appMainHeader: MainHeader,
+  },
+  methods: {
+    newTodo() {},
   },
 };
 </script>
 
 <style>
 .container {
-  background-color: bisque;
+  background-color: white;
   flex: 1;
+}
+
+.inputContainer {
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-self: stretch;
+}
+
+.input {
+  flex: 1;
+  height: 35px;
+  background-color: #f3f3f3;
+  font-size: 18px;
+  color: #888888;
+}
+
+.add-btn {
+  width: 100px;
+  height: 35px;
+  background-color: #ffce00;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-text {
+  font-size: 18px;
+  font-weight: 700;
 }
 </style>
