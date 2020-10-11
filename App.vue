04d69c6116a9ae412e0af1f0861d3ee149386469
Aniket <aniketicloud@gmail.com>
2020-10-11 18:47:36 +0530

    added to do List component

--- App.vue
+++ App.vue
@@ -3,6 +3,7 @@
     <app-status-bar color="#ffce00" />
     <app-main-header title="to-do-app"></app-main-header>
     <app-new-to-do></app-new-to-do>
+    <app-to-do-list></app-to-do-list>
   </view>
 </template>
 
@@ -10,12 +11,14 @@
 import StatusBar from "./components/status-bar/StatusBar";
 import MainHeader from "./components/mainheader/MainHeader";
 import NewToDo from "./components/new-to-do/NewToDo";
+import ToDoList from "./components/to-do-list/ToDoList";
 
 export default {
   components: {
     appStatusBar: StatusBar,
     appMainHeader: MainHeader,
     appNewToDo: NewToDo,
+    appToDoList: ToDoList,
   },
 };
 </script>
---
+++ components/to-do-list/ToDoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <view>
+    <text class="todolist" v-for="todo in todos" :key="todo.id">
+      <touchable-opacity :on-press="() => toggleDone(todo.id)">
+        <text class="todo-text done" v-if="todo.done">{{ todo.title }}</text>
+        <text class="todo-text" v-else>{{ todo.title }}</text>
+      </touchable-opacity>
+
+      <touchable-opacity
+        class="remove-btn"
+        :on-press="() => removeTodo(todo.id)"
+      >
+        <text class="remove-btn-text">Remove</text>
+      </touchable-opacity>
+    </text>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "Complete Vue native",
+          done: false,
+        },
+        {
+          id: 1,
+          title: "Revise your Chinese",
+          done: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    toggleDone() {},
+    removeTodo() {},
+  },
+};
+</script>
+
+<style scoped>
+.todolist {
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px;
+}
+
+.todo-text {
+  font-size: 18px;
+}
+
+.done {
+  color: chartreuse;
+}
+
+.remove-btn-text {
+  font-size: 18px;
+  color: crimson;
+}
+</style>
