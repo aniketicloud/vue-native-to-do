130d7f15ffbee0054985ed8fec6e97f183324c6b
Aniket <aniketicloud@gmail.com>
2020-10-11 17:15:29 +0530

    separated new to do component

--- App.vue
+++ App.vue
@@ -2,32 +2,20 @@
   <view class="container">
     <app-status-bar color="#ffce00" />
     <app-main-header title="to-do-app"></app-main-header>
-
-    <view class="inputContainer">
-      <text-input class="input" v-model="newTodoText"></text-input>
-      <touchable-opacity class="add-btn" :onPress="newTodo">
-        <text class="btn-text">ADD</text>
-      </touchable-opacity>
-    </view>
+    <app-new-to-do></app-new-to-do>
   </view>
 </template>
 
 <script>
 import StatusBar from "./components/status-bar/StatusBar";
 import MainHeader from "./components/mainheader/MainHeader";
+import NewToDo from "./components/new-to-do/NewToDo";
 
 export default {
-  data() {
-    return {
-      newTodoText: "",
-    };
-  },
   components: {
     appStatusBar: StatusBar,
     appMainHeader: MainHeader,
-  },
-  methods: {
-    newTodo() {},
+    appNewToDo: NewToDo,
   },
 };
 </script>
@@ -36,33 +24,5 @@ export default {
 .container {
   background-color: white;
   flex: 1;
-}
-
-.inputContainer {
-  width: 100%;
-  flex-direction: row;
-  justify-content: center;
-  align-self: stretch;
-}
-
-.input {
-  flex: 1;
-  height: 35px;
-  background-color: #f3f3f3;
-  font-size: 18px;
-  color: #888888;
-}
-
-.add-btn {
-  width: 100px;
-  height: 35px;
-  background-color: #ffce00;
-  justify-content: center;
-  align-items: center;
-}
-
-.btn-text {
-  font-size: 18px;
-  font-weight: 700;
 }
 </style>
---
+++ components/new-to-do/NewToDo.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="inputContainer">
+    <text-input class="input" v-model="newTodoText"></text-input>
+    <touchable-opacity class="add-btn" :onPress="newTodo">
+      <text class="btn-text">ADD</text>
+    </touchable-opacity>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoText: "",
+    };
+  },
+
+  methods: {
+    newTodo() {},
+  },
+};
+</script>
+
+<style scoped>
+.inputContainer {
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-self: stretch;
+}
+
+.input {
+  flex: 1;
+  height: 35px;
+  background-color: #f3f3f3;
+  font-size: 18px;
+  color: #888888;
+}
+
+.add-btn {
+  width: 100px;
+  height: 35px;
+  background-color: #ffce00;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-text {
+  font-size: 18px;
+  font-weight: 700;
+}
+</style>
